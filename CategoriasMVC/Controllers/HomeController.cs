518c049dcd080a800763cb2fbbd8402675ba3d9c
carloshvidigal <chmovi@hotmail.com>
2025-11-19 13:26:43 -0300

    Add project files.

---
+++ CategoriasMVC/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+using CategoriasMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoriasMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
